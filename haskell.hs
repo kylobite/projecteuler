15c2000b59c666d65dd975f5e5b1359e224dc65c
kylobite <kylolink@gmail.com>
2014-03-25 17:26:03 -0500

    Problem #2

--- haskell.hs
+++ haskell.hs
@@ -1,2 +1,8 @@
 -- Problem 1
 p1 = sum $ filter (\x->rem 3 == 0 || rem x 5 == 0) [1..999]
+
+-- Problem 2
+fib a b = [a,b] ++ fib' b (a + b) where
+    fib' a b = [b] ++ fib' b (a + b)
+
+p2 = sum $ filter even $ takeWhile (<=4000000) $ fib 1 2
