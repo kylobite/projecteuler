f056d57c35005a1897716e70cb8c4f293988dc85
kylobite <kylolink@gmail.com>
2014-03-25 22:01:59 -0500

    Problem #3

--- haskell.hs
+++ haskell.hs
@@ -8,23 +8,7 @@ fib a b = [a,b] ++ fib' b (a + b) where
 p2 = sum $ filter even $ takeWhile (<=4000000) $ fib 1 2
 
 -- Problem 3
--- Please forgive me for the code that follows
-primes = [2] ++ filter (\x -> not $ or $ map (\y -> rem x y == 0) [2..x - 1]) [3,5..]
-
-p3 x = let plist = takeWhile (<=(div x 2)) primes
-       in last $ check x plist [] where
-        check :: Int -> [Int] -> [Int] -> [Int]
-        check x [] c     = c
-        check x (p:ps) c = if rem x p == 0
-                            then let r = reduce x p []
-                                     y = fst r
-                                     d = snd r
-                                 in if y > 1
-                                        then check y ps (c ++ d)
-                                        else (c ++ d)
-                            else check x ps c
-        reduce a b c = if rem a b == 0
-                        then if a `div` b > 1
-                                then reduce (div a b) b (b:c)
-                                else ((div a b),(a:c))
-                        else (a,c)
+p3 n = p3' n 2 where
+    p3' n x | n == x = x 
+            | n `mod` x == 0 = p3' (n `div` x) x
+            | otherwise = p3' n (succ x)  
