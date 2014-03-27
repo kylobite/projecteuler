231cbeb87623431cd39b6623ce7ef60759b93108
kylobite <kylolink@gmail.com>
2014-03-26 23:15:03 -0500

    Problem #4

--- euler.hs
+++ euler.hs
@@ -14,13 +14,12 @@ p3 n = p3' n 2 where
             | otherwise = p3' n (succ x)  
 
 -- Problem 4
--- 10001 - 997799
 -- Generate palindromes
 palindromes = reverse $ make1 ++ make2 where
     -- 10001-99999
-    make1 = concat $ map (\y -> map (\x -> x ++ y ++ reverse x) $ map show [10..99]) $ map show [0..9]     
+    make1 = concat $ map (\y -> map (\x -> y ++ x ++ reverse y) $ map show [0..9]) $ map show [10..99]     
     -- 100001-997799 
-    make2 = concat $ map (\y -> map (\x -> x ++ y ++ y ++ reverse x) $ map show [10..99]) $ map show [0..7]
+    make2 = concat $ map (\y -> map (\x -> y ++ x ++ x ++ reverse y) $ map show [0..7]) $ map show [10..99]
 
 -- Check if number is three digits and the quotient is three digits
 using3 a b = let m = rem a b
@@ -28,3 +27,15 @@ using3 a b = let m = rem a b
              in if m == 0 && (length $ show n) == 3
                 then True
                 else False
+
+p4  = let p = map (\x -> read x :: Int) palindromes
+          d = reverse [100..999]
+      in f p d where
+        f :: [Int] -> [Int] -> Int
+        f [] _     = 0
+        f (x:xs) y = let n = filter (\z -> using3 x z) y
+                     in if length n > 0
+                        then x
+                        else f xs y
+
+-- Problem 5
